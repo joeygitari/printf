cc6083876438eac7f2986154e0ceb043a9f0ce18
Joanne Gitari <75974016+joeygitari@users.noreply.github.com>
2023-01-28 10:03:42 +0300

    Create printoct.c

---
+++ printoct.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_oct - prints integer as octal
+ * @ap: int to convert to octal
+ * Return: number of binary characters printed
+ */
+int print_oct(va_list ap)
+
+{
+	unsigned int i;
+
+	i = va_arg(ap, unsigned int);
+	return (p_conv(i, 8, 1));
+}
